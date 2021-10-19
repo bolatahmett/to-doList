fb060c8d308cd98dd7f27cfaec94aaf3ae12ba3e
AhmetBolat <ahmet.bolat@invicti.com>
2021-10-20 00:26:09 +0300

    ui code refactoring

--- Source/UI/app/index.js
+++ Source/UI/app/index.js
@@ -7,6 +7,44 @@ const App = () => {
     const [list, setList] = useState([]);
     const [message, setMessage] = useState('');
 
+    const addTask = () => {
+        const newList = list.concat({ Message: message, IsSelected: false });
+        setList(newList);
+        setMessage('');
+    }
+
+    const removeTask = () => {
+        const newList = list.filter((item) => !item.IsSelected);
+        setList(newList);
+    }
+
+    const handleListItemClick = (e) => {
+        const newList = list.map((item, index) => {
+            if (index.toString() === e.target.getAttribute("data-index")) {
+                const updatedItem = {
+                    ...item,
+                    IsSelected: !item.IsSelected,
+                };
+
+                return updatedItem;
+            }
+
+            return item;
+        });
+
+        setList(newList);
+    }
+
+    const taskList = list.map((item, index) => (
+        <li className={item.IsSelected ? "selected-task-item" : "task-item"}
+            data-index={index}
+            key={index}
+            onClick={handleListItemClick}
+        >
+            {item.Message}
+        </li>
+    ));
+
     return (
         <>
             <input
@@ -15,43 +53,12 @@ const App = () => {
                 value={message}
                 onChange={(val) => { setMessage(val.target.value) }} />
 
-            <button id="addButton" onClick={() => {
-                const newList = list.concat({ Message: message, IsSelected: false });
-                setList(newList);
-                setMessage('');
-            }}> Add Button
-            </button>
+            <button id="addButton" onClick={addTask}> Add Button </button>
 
-            <button id="removeButton" onClick={() => {
-                const newList = list.filter((item) => !item.IsSelected);
-                setList(newList);
-            }}> Remove Button
-            </button>
+            <button id="removeButton" onClick={removeTask}> Remove Button </button>
 
             <ul id="taskList">
-                {list.map((item, index) => (
-                    <li className={item.IsSelected ? "selected-task-item" : "task-item"}
-                        data-index={index}
-                        key={index}
-                        onClick={(e) => {
-
-                            const newList = list.map((item, index) => {
-                                if (index.toString() === e.target.getAttribute("data-index")) {
-                                    const updatedItem = {
-                                        ...item,
-                                        IsSelected: !item.IsSelected,
-                                    };
-
-                                    return updatedItem;
-                                }
-
-                                return item;
-                            });
-
-                            setList(newList);
-                        }}
-                    >{item.Message}</li>
-                ))}
+                {taskList}
             </ul>
         </>
     )
